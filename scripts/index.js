d928515659de03310fc1159057c271b3bf73d7b6
Nickolai <lindeman072@mail.ru>
2023-03-17 16:20:22 +0400

    Add picture to project

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,16 @@ const nameInput = formElement.querySelector(".popup__name_type_name");
 const jobInput = formElement.querySelector(".popup__job_type_job");
 const valueNameInput = document.querySelector(".section-title");
 const valueJobInput = document.querySelector(".section-subtitle");
+const popupOpenAddPictureButtonElement = document.querySelector(".profile__add-button");
+const popupElementAddPicture = document.querySelector(".popup__addPicture");
+const popupCloseButtonAddPicture = document.querySelector(".popup__close_addPicture");
+const heartBlack = document.querySelector(".elements__button");
+
+const paintBlackHeart = function () {
+  heartBlack.classList.toggle("elements__button_active");
+}
+heartBlack.addEventListener("click", paintBlackHeart);
+
 const openPopup = function () {
   nameInput.value = valueNameInput.textContent;
   jobInput.value = valueJobInput.textContent;
@@ -30,3 +40,13 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 formElement.addEventListener("submit", handleFormSubmit);
+// addPicture
+
+const addPicture = function () {
+  popupElementAddPicture.classList.add("popup__addPicture_opened");
+};
+const closeAddPicture = function () {
+  popupElementAddPicture.classList.remove("popup__addPicture_opened");
+};
+popupOpenAddPictureButtonElement.addEventListener("click", addPicture);
+popupCloseButtonAddPicture.addEventListener("click", closeAddPicture);
