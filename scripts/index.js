085108a9c8c4bfce95ae544ebf3666d5d1eb748b
Nickolai <lindeman072@mail.ru>
2023-04-04 21:22:51 +0400

    problem with formes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 // Переменные страницы
+const popupElements = document.querySelectorAll(".popup");
 const profileOpenButton = document.querySelector(".profile__edit-button");
 const profileAddButton = document.querySelector(".profile__add-button");
 const valueNameInput = document.querySelector(".section-title");
@@ -26,13 +27,31 @@ const itemTemplate = document.querySelector("#template").content;
 // Открытие/закрытие попапов
 function openPopup(popup) {
   popup.classList.add("popup_opened");
+  document.addEventListener("keydown", closePopupOnEsc);
 }
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", closePopupOnEsc);
 }
+// Функция закрытия попапа по Escape
+function closePopupOnEsc(evt) {
+  if (evt.key === "Escape") {
+    const popup = document.querySelector(".popup_opened");
+    closePopup(popup);
+  }
+}
+// Функция закрытия попапа по оверлею
+const closePopupOnOverlay = function (event) {
+  if (event.target === event.currentTarget) {
+    closePopup(event.target);
+  }
+};
+popupElements.forEach((item) => {
+  item.addEventListener("click", closePopupOnOverlay);
+});
 closeButtons.forEach((button) => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopup(popup));
+  const popup = button.closest(".popup");
+  button.addEventListener("click", () => closePopup(popup));
 });
 // Открытие попапа для профиля
 profileOpenButton.addEventListener("click", function () {
@@ -130,3 +149,75 @@ function addCardOnPage(event) {
   addOnPage(createCard(newPicture));
   pictureForm.reset();
 }
+
+
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_invalid',
+  activeButtonClass: 'popup__submit-button_valid',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+// Подготовка формы
+const enableValidation = ({ formSelector, ...rest }) => {
+  const forms = Array.from(document.querySelectorAll(formSelector));
+forms.forEach(form => {
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    })
+  setEventListeners(form, rest)
+})
+}
+// Навешиваем слушатели инпутам
+const setEventListeners = (formToValidate, { inputSelector, submitButtonSelector, ...rest }) => {
+  const formInputs = Array.from(formToValidate.querySelectorAll(inputSelector));
+  const formButton = formToValidate.querySelector(submitButtonSelector);
+  formInputs.forEach(input => {
+    disableButton(formButton, rest);
+    input.addEventListener("input", () => {
+      checkInputValidity(input);
+      if (hasInvalidInput(formInputs)) {
+        disableButton(formButton, rest);
+           } else {
+            enableButton(formButton, rest)
+          }
+      });
+})
+}
+const showErrorMessage = function (currentInputErrorContainer, input) {
+  currentInputErrorContainer.classList.add("popup__error_visible")
+  currentInputErrorContainer.textContent = input.validationMessage
+  input.classList.add("popup__input_type_error")
+}
+const hideErrorMessage = function (currentInputErrorContainer, input) {
+  currentInputErrorContainer.classList.remove("popup__error_visible")
+  currentInputErrorContainer.textContent = ''
+  input.classList.remove("popup__input_type_error")
+}
+// Проверка валидности инпутов
+const checkInputValidity = (input) => {
+  const currentInputErrorContainer = document.querySelector(`#${input.id}-error`);
+if (input.checkValidity()) {
+  hideErrorMessage(currentInputErrorContainer, input);
+} else {
+  showErrorMessage(currentInputErrorContainer, input);
+}
+}
+const hasInvalidInput = (formInputs) => {
+  return formInputs.some(item => !item.validity.valid)
+}
+// Активнеая/неактивная кнопка
+const enableButton = (button, { inactiveButtonClass, activeButtonClass }) => {
+  button.classList.remove(inactiveButtonClass);
+  button.classList.add(activeButtonClass);
+  button.removeAttribute("disabled", "disabled");
+}
+const disableButton = (button, { inactiveButtonClass, activeButtonClass }) => {
+  button.classList.add(inactiveButtonClass);
+  button.classList.remove(activeButtonClass);
+  button.setAttribute("disabled", "disabled");
+}
+enableValidation(validationConfig);
